15d62e4a37d50fcff1a9fb06d5b25f00d513c055
Paul Njoroge <njorogepaul418@gmail.com>
2026-06-19 13:54:47 +0300

    Adding re

---
+++ re.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void person(char *name){
+printf("My name is %s\n",name);
+}
+
+void profit(int x,int y){
+printf("we made gross profit of %d\n",x*y);
+}
+
+void fin(char *mes){
+printf("We thank you all %s\n",mes);
+}
+
+
+int main(){
+void (*rperson)(char *p);
+rperson=&person;
+rperson("Paul");
+
+void (*rprofit)(int x,int y);
+rprofit=&profit;
+rprofit(12,40000);
+
+void (*rfin)(char *f);
+rfin=&fin;
+rfin("Employees");
+return 0;
+}
